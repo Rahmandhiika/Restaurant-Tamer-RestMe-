67d3f62237427eaf318af985f188f5edbcfe695d
Rahmandhika Putra Purwadi Wicaksono <rahmandhika19@gmail.com>
2026-09-05 13:53:13 +0700

    Initial Commit

---
+++ RestMe/RestMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestMeApp.swift
+//  RestMe
+//
+//  Created by Rahmandhika Putra Purwdi Wicaksono on 05/09/26.
+//
+
+import SwiftUI
+
+@main
+struct RestMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
